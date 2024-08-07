a7a03c93ddc0425dcc1162ad9942c6df1a9ab078
Harsh48 <harsh.techilia@gmail.com>
2024-08-08 00:35:54 +0530

    fix:logging in api call

--- src/services/quizService.ts
+++ src/services/quizService.ts
@@ -1,6 +1,10 @@
 import axios from 'axios';
 
 export const fetchQuizQuestions = async (category: string, difficulty: string) => {
-  const response = await axios.get(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}`);
-  return response.data.results;
+  try{
+    const response = await axios.get(`https://opentdb.com/api.php?amount=10&category=${category}&difficulty=${difficulty}`);
+    return response.data.results;
+  }catch(e){
+    console.log(e)
+  }
 };
